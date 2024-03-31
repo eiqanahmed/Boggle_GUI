96ca995e0561b41f9bbdb0c97044cd2257cc4f7d
Eiqan <117050195+eiqanahmed@users.noreply.github.com>
2024-03-31 18:51:29 -0400

    Create Braille_or_Eng.java

---
+++ State/Braille_or_Eng.java
@@ -0,0 +1,11 @@
+package State;
+
+public class Braille_or_Eng {
+    public State state = null;
+    public void Braille_or_Eng (){
+        this.state = null;
+    }
+    public void setState(State state){
+        this.state = state;
+    }
+}
